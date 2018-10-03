2f7549447a9ff722c480bca6e686e3695e4bc652
github username <github email address>
2018-10-02 20:12:13 -0400

    deluxe create finished

--- game_back_end/app/controllers/players_controller.rb
+++ game_back_end/app/controllers/players_controller.rb
@@ -15,10 +15,20 @@ class PlayersController < ActionController::API
     render json: @player, include: ['games']
   end
   
-  def create 
+  # def create 
+  #   @player = Player.new(player_params)
+  #     if @player.save
+  #       render json: @player, status: :created, location: @player
+  #     else
+  #       render json: @player.errors, status: :uprocessable_entity
+  #     end
+  # end
+
+  def create_both 
     @player = Player.new(player_params)
       if @player.save
-        render json: @player, status: :created, location: @player
+        @player.games << Game.create!(player_id: @player.id, highest_level: params[:highest_level])
+        render json: @player, include: ['games']
       else
         render json: @player.errors, status: :uprocessable_entity
       end
@@ -30,6 +40,6 @@ class PlayersController < ActionController::API
 
   private
   def player_params 
-    params.require(:player).permit(:name)
+    params.require(:player).permit(:name, :highest_level)
   end
 end
--- game_back_end/app/models/player.rb
+++ game_back_end/app/models/player.rb
@@ -1,6 +1,9 @@
 class Player < ApplicationRecord
   has_many :games
   validates :name, presence: true
+
+  attr_accessor :highest_level
+  
   def top_score
     self.games.sort{|a, b|
       b.highest_level <=> a.highest_level
